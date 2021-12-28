cd6d35ab7339642481b70f8181c94de9475a406b
yamaarash1 <39236992+yamaarash1@users.noreply.github.com>
2021-12-28 01:54:56 +0000

    Klab Tech 1 day

--- app/model.py
+++ app/model.py
@@ -37,14 +37,20 @@ def create_user(name: str, leader_card_id: int) -> str:
             ),
             {"name": name, "token": token, "leader_card_id": leader_card_id},
         )
-        # print(result)
+        #print(result)
     return token
 
 
 def _get_user_by_token(conn, token: str) -> Optional[SafeUser]:
-    # TODO: 実装
-    pass
-
+    result = conn.execute(
+       text("SELECT `id`, `name`, `leader_card_id` FROM `user` WHERE `token`=:token"),
+       dict(token=token),
+    )
+    try:
+        row = result.one()
+    except NoResultFound:
+        return None
+    return SafeUser.from_orm(row)
 
 def get_user_by_token(token: str) -> Optional[SafeUser]:
     with engine.begin() as conn:

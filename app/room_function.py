3264d338cddd3e1b91895fef2a293365eec07e7d
yamaarash1 <39236992+yamaarash1@users.noreply.github.com>
2022-01-05 05:28:06 +0000

    fix room join api

--- app/room_function.py
+++ app/room_function.py
@@ -45,23 +45,26 @@ def room_list_get(live_id: int) -> list[RoomInfo]:
 
 
 def _room_join(conn, room_id: int, select_difficulty: LiveDifficulty, user: SafeUser):
-    result = conn.execute(
-        text("INSERT INTO `join_user` (difficulty, user_id, room_id) VALUES (:select_difficulty, :user_id, :room_id)"),
-        {"select_difficulty": select_difficulty.value, "user_id": user.id, "room_id": room_id},
-    )
-    room = conn.execute(
-        text("SELECT `id` FROM `room` WHERE `id`= :room_id"),
-        {"room_id": room_id},
-    )
     try:
-        if(room.is_dissolution):
+        result = conn.execute(
+            text("SELECT id, joined_user_account, max_user_count, is_dissolution FROM `room` WHERE `id`= :room_id"),
+            {"room_id": room_id},
+        )
+        res = result.one()
+        room = {"id": res[0], "joined_user_account": res[1], "max_user_count": res[2], "is_dissolution": res[3]}
+        print(room)
+        if(room["is_dissolution"]):
             res = {"join_room_result": 3}
         else:
-            if(room.joined_user_account >= room.max_user_count):
+            if(room["joined_user_account"] < room["max_user_count"]):
+                conn.execute(
+                    text("INSERT INTO `join_user` (difficulty, user_id, room_id) VALUES (:select_difficulty, :user_id, :room_id)"),
+                    {"select_difficulty": select_difficulty.value, "user_id": user.id, "room_id": room_id},
+                )
                 res = {"join_room_result": 1}
                 conn.execute(
                     text("UPDATE `room` SET joined_user_account = :joined_user_account where id = :room_id"),
-                    {"joined_user_account": room.joined_user_account + 1, room_id: room.id}
+                    {"joined_user_account": room["joined_user_account"] + 1, "room_id": room["id"]},
                 )
             else:
                 res = {"join_room_result": 2}

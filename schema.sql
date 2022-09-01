1071388bdb6f9beac5d06470bac09d26b4300637
Hiroya-W <hyuyukun@gmail.com>
2022-09-01 07:01:20 +0000

    :bug: Set NOT NULL for name, token, leader_card_id

--- schema.sql
+++ schema.sql
@@ -1,9 +1,9 @@
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
   `id` bigint NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) DEFAULT NULL,
-  `token` varchar(255) DEFAULT NULL,
-  `leader_card_id` int DEFAULT NULL,
+  `name` varchar(255) NOT NULL,
+  `token` varchar(255) NOT NULL,
+  `leader_card_id` int NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `token` (`token`)
 );

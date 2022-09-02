132c77b6b232d29070117c099007d58841fd3f92
matac <40331709+matac42@users.noreply.github.com>
2022-09-02 06:56:51 +0000

    feat: create room model

--- app/model.py
+++ app/model.py
@@ -10,7 +10,7 @@ from sqlalchemy.exc import NoResultFound
 
 from .db import engine
 
-
+# User
 class InvalidToken(Exception):
     """指定されたtokenが不正だったときに投げる"""
 
@@ -29,6 +29,50 @@ class SafeUser(BaseModel):
         orm_mode = True
 
 
+# Room
+# Enum
+class LiveDifficulty(Enum):
+    normal = 1
+    hard = 2
+
+
+class JoinRoomResult(Enum):
+    Ok = 1
+    RoomFull = 2
+    Disbanded = 3
+    OtherError = 4
+
+
+class WaitRoomStatus(Enum):
+    Waiting = 1
+    LiveStart = 2
+    Dissolution = 3
+
+
+# Model
+class RoomInfo(BaseModel):
+    id: int
+    live_id: int
+    joined_user_count: int
+    max_user_count: int
+
+
+class RoomUser(BaseModel):
+    user_id: int
+    name: str
+    leader_card_id: int
+    select_difficulty: LiveDifficulty
+    is_me: bool
+    is_host: bool
+
+
+class ResultUser(BaseModel):
+    user_id: int
+    judge_count_list: list[int]
+    score: int
+
+
+# User
 def create_user(name: str, leader_card_id: int) -> str:
     """Create new user and returns their token"""
     token = str(uuid.uuid4())
@@ -70,3 +114,39 @@ def update_user(token: str, name: str, leader_card_id: int) -> None:
             dict(token=token, name=name, leader_card_id=leader_card_id),
         )
         print(result)
+
+
+# Room
+def create_room(token: str, live_id: int, select_difficulty: LiveDifficulty) -> int:
+    """Create new room and returns their id"""
+    with engine.begin() as conn:
+        result = conn.execute(
+            text("INSERT INTO `room` (live_id) VALUES (:live_id)"),
+            {"live_id": live_id},
+        )
+    room_id = result.lastrowid
+    _create_room_member(token, room_id, select_difficulty, True)
+
+    return room_id
+
+
+def _create_room_member(
+    token: str, room_id: int, select_difficulty: LiveDifficulty, is_host: bool
+) -> None:
+    user = get_user_by_token(token)
+    with engine.begin() as conn:
+        result = conn.execute(
+            text(
+                "INSERT INTO `room_member` (room_id, user_id, name, leader_card_id, select_difficulty, is_host) \
+                VALUES (:room_id, :user_id, :name, :leader_card_id, :select_difficulty, :is_host)"
+            ),
+            {
+                "room_id": room_id,
+                "user_id": user.id,
+                "name": user.name,
+                "leader_card_id": user.leader_card_id,
+                "select_difficulty": select_difficulty,
+                "is_host": is_host,
+            },
+        )
+    print(result)
--- schema.sql
+++ schema.sql
@@ -1,9 +1,28 @@
 DROP TABLE IF EXISTS `user`;
+DROP TABLE IF EXISTS `room`;
 CREATE TABLE `user` (
   `id` bigint NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) DEFAULT NULL,
-  `token` varchar(255) DEFAULT NULL,
-  `leader_card_id` int DEFAULT NULL,
+  `name` varchar(255) NOT NULL,
+  `token` varchar(255) NOT NULL,
+  `leader_card_id` int NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `token` (`token`)
+);
+CREATE TABLE `room` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `live_id` int NOT NULL,
+  `joined_user_count` int NOT NULL DEFAULT 1,
+  `max_user_count` int NOT NULL DEFAULT 4,
+  PRIMARY KEY (`id`)
+);
+CREATE TABLE `room_member` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `room_id` bigint NOT NULL,
+  `user_id` int NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `leader_card_id` int NOT NULL,
+  `select_difficulty` int NOT NULL,
+  `is_host` boolean NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `user_id` (`user_id`)
 );

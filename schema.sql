752d51bc643c4c7e51fabe71235bf71941636358
Kotokaze <62094392+kotokaze@users.noreply.github.com>
2022-09-02 07:47:23 +0000

    :sparkles: Add room schema

--- schema.sql
+++ schema.sql
@@ -7,3 +7,21 @@ CREATE TABLE `user` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `token` (`token`)
 );
+
+DROP TABLE IF EXISTS `room`;
+CREATE TABLE `room` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `live_id` int NOT NULL,
+  `host_id` bigint NOT NULL,
+  `status` int NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+DROP TABLE IF EXISTS `room_user`;
+CREATE TABLE `room_user` (
+  `room_id` bigint NOT NULL,
+  `user_id` bigint NOT NULL,
+  `difficulty` tinyint NOT NULL,
+  `score` int NOT NULL DEFAULT 0,
+  PRIMARY KEY (`room_id`, `user_id`)
+);

60504e38c996bb43dd954f5384a9f29a6b99106e
Taukon <83957880+Taukon@users.noreply.github.com>
2022-09-02 04:51:20 +0000

    複数の関数に修正

--- tests/test_user.py
+++ tests/test_user.py
@@ -5,7 +5,7 @@ from app.api import app
 client = TestClient(app)
 
 
-def test_create_user():
+def test_create_user__成功():
     response = client.post(
         "/user/create", json={"user_name": "test1", "leader_card_id": 1000}
     )
@@ -24,9 +24,30 @@ def test_create_user():
     assert response_data["name"] == "test1"
     assert response_data["leader_card_id"] == 1000
 
+
+def test_update_user__成功():
+    response = client.post(
+        "/user/create", json={"user_name": "test_update", "leader_card_id": 1000}
+    )
+    assert response.status_code == 200
+
+    response_data = response.json()
+    assert response_data.keys() == {"user_token"}
+
+    token = response.json()["user_token"]
+
+    response = client.get("/user/me", headers={"Authorization": f"bearer {token}"})
+    assert response.status_code == 200
+
+    response_data = response.json()
+    assert response_data.keys() == {"id", "name", "leader_card_id"}
+
     response = client.post(
         "/user/update",
-        json={"user_name": "test2", "leader_card_id": response_data["leader_card_id"]},
+        json={
+            "user_name": "test_update2",
+            "leader_card_id": response_data["leader_card_id"],
+        },
         headers={"Authorization": f"bearer {token}"},
     )
     assert response.status_code == 200
@@ -36,5 +57,34 @@ def test_create_user():
 
     response_data = response.json()
     assert response_data.keys() == {"id", "name", "leader_card_id"}
-    assert response_data["name"] == "test2"
+    assert response_data["name"] == "test_update2"
     assert response_data["leader_card_id"] == 1000
+
+
+def test_update_user__tokenなしエラー():
+    response = client.post(
+        "/user/create", json={"user_name": "test_update", "leader_card_id": 1000}
+    )
+    assert response.status_code == 200
+
+    response_data = response.json()
+    assert response_data.keys() == {"user_token"}
+
+    token = response.json()["user_token"]
+
+    response = client.get("/user/me", headers={"Authorization": f"bearer {token}"})
+    assert response.status_code == 200
+
+    response_data = response.json()
+    assert response_data.keys() == {"id", "name", "leader_card_id"}
+
+    response_data = response.json()
+
+    response = client.post(
+        "/user/update",
+        json={
+            "user_name": "test_update2",
+            "leader_card_id": response_data["leader_card_id"],
+        },
+    )
+    assert response.status_code == 403

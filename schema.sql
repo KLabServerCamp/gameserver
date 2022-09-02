f31cc7271a18ec9281261f5fee4c92112576f8fa
Hiroya-W <hyuyukun@gmail.com>
2022-09-02 05:23:08 +0000

    :sparkles: Defined room, room_member table scheme

--- schema.sql
+++ schema.sql
@@ -1,9 +1,29 @@
 DROP TABLE IF EXISTS `user`;
+DROP TABLE IF EXISTS `room`;
+DROP TABLE IF EXISTS `room_member`;
+
 CREATE TABLE `user` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `token` varchar(255) NOT NULL,
-  `leader_card_id` int NOT NULL,
+  `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NOT NULL,
+  `token` VARCHAR(255) NOT NULL,
+  `leader_card_id` INT UNSIGNED NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `token` (`token`)
+);
+
+CREATE TABLE `room` (
+  `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `room_id` BIGINT UNSIGNED NOT NULL,
+  `live_id` INT UNSIGNED NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `room_id` (`room_id`)
+);
+
+CREATE TABLE `room_member` (
+  `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `room_id` BIGINT UNSIGNED NOT NULL,
+  `user_id` BIGINT UNSIGNED NOT NULL,
+  `is_owner` INT UNSIGNED NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `room_id` (`room_id`, `user_id`)
 );

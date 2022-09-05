f54a9313112d68f2b387babe04a1c796cbfdb179
Hiroya-W <hyuyukun@gmail.com>
2022-09-05 08:16:49 +0000

    :recycle: Move user api in user.py

--- app/api.py
+++ app/api.py
@@ -1,4 +1,4 @@
-from fastapi import Depends, FastAPI, HTTPException
+from fastapi import Depends, FastAPI
 from pydantic import BaseModel
 
 from . import model
@@ -9,12 +9,12 @@ from .model import (
     ResultUser,
     RoomInfo,
     RoomUser,
-    SafeUser,
     WaitRoomStatus,
 )
+from .routers import user
 
 app = FastAPI()
-
+app.include_router(user.router)
 
 # Sample APIs
 
@@ -24,16 +24,8 @@ async def root() -> dict[str, str]:
     return {"message": "Hello World"}
 
 
-# User APIs
-
-
-class UserCreateRequest(BaseModel):
-    user_name: str
-    leader_card_id: int
-
-
-class UserCreateResponse(BaseModel):
-    user_token: str
+class Empty(BaseModel):
+    pass
 
 
 class RoomCreateRequest(BaseModel):
@@ -209,34 +201,6 @@ class RoomLeaveRequest(BaseModel):
     """
 
     room_id: int
-
-
-@app.post("/user/create", response_model=UserCreateResponse)
-def user_create(req: UserCreateRequest) -> UserCreateResponse:
-    """新規ユーザー作成"""
-    token = model.create_user(req.user_name, req.leader_card_id)
-    return UserCreateResponse(user_token=token)
-
-
-@app.get("/user/me", response_model=SafeUser)
-def user_me(token: str = Depends(get_auth_token)) -> SafeUser:
-    user = model.get_user_by_token(token)
-    if user is None:
-        raise HTTPException(status_code=404)
-    # print(f"user_me({token=}, {user=})")
-    return user
-
-
-class Empty(BaseModel):
-    pass
-
-
-@app.post("/user/update", response_model=Empty)
-def update(req: UserCreateRequest, token: str = Depends(get_auth_token)) -> dict:
-    """Update user attributes"""
-    # print(req)
-    model.update_user(token, req.user_name, req.leader_card_id)
-    return {}
 
 
 # Room APIs

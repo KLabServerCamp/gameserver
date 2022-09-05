6bd60fe07bfb4a7f1f569566db5905bb74214a23
xenonMotsu <45729807+xenonMotsu@users.noreply.github.com>
2022-09-05 08:24:40 +0000

    トークンの衝突回避ルーチンを追加

--- app/model.py
+++ app/model.py
@@ -29,22 +29,31 @@ class SafeUser(BaseModel):
 
 def create_user(name: str, leader_card_id: int) -> str:
     """Create new user and returns their token"""
-    token = str(uuid.uuid4())
-    # NOTE: tokenが衝突したらリトライする必要がある.
     with engine.begin() as conn:
-        result = conn.execute(
-            text(
-                "INSERT INTO `user` (name, token, leader_card_id) VALUES (:name, :token, :leader_card_id)"
-            ),
-            {"name": name, "token": token, "leader_card_id": leader_card_id},
-        )
-        # print(result)
+        while True:
+            token = str(uuid.uuid4())
+            result = conn.execute(
+                text(
+                    "SELECT token FROM user WHERE token = :token"
+                ),
+                {"token": token},
+            )
+            row_or_None = result.one_or_none()
+            if row_or_None is not None:
+                continue
+            result = conn.execute(
+                text(
+                    "INSERT INTO `user` (name, token, leader_card_id) VALUES (:name, :token, :leader_card_id)"
+                ),
+                {"name": name, "token": token, "leader_card_id": leader_card_id},
+            )
+            break
     return token
 
 
 def _get_user_by_token(conn, token: str) -> Optional[SafeUser]:
     res = conn.execute(
-        text("select id, name, leader_card_id from user where token = :token"),
+        text("SELECT id, name, leader_card_id FROM user WHERE token = :token"),
         {"token": token},
     )
     try:

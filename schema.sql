6c191f7afa9ef8501b0d72dcd4a23f3d9380cd3c
Kotokaze <62094392+kotokaze@users.noreply.github.com>
2022-09-05 04:56:33 +0000

    :white_check_mark: Use foreign key

--- schema.sql
+++ schema.sql
@@ -1,4 +1,7 @@
+DROP TABLE IF EXISTS `room_user`;
+DROP TABLE IF EXISTS `room`;
 DROP TABLE IF EXISTS `user`;
+
 CREATE TABLE `user` (
   `id` bigint NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
@@ -8,20 +11,21 @@ CREATE TABLE `user` (
   UNIQUE KEY `token` (`token`)
 );
 
-DROP TABLE IF EXISTS `room`;
 CREATE TABLE `room` (
   `id` bigint NOT NULL AUTO_INCREMENT,
   `live_id` int NOT NULL,
   `host_id` bigint NOT NULL,
   `status` int NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`host_id`) REFERENCES `user` (`id`)
 );
 
-DROP TABLE IF EXISTS `room_user`;
 CREATE TABLE `room_user` (
   `room_id` bigint NOT NULL,
   `user_id` bigint NOT NULL,
   `difficulty` tinyint NOT NULL,
   `score` int NOT NULL DEFAULT 0,
-  PRIMARY KEY (`room_id`, `user_id`)
+  PRIMARY KEY (`room_id`, `user_id`),
+  FOREIGN KEY (`room_id`) REFERENCES `room` (`id`),
+  FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
 );

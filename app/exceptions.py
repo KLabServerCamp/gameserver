1897537f6488f154578347e225b904661426b92a
Hiroya-W <hyuyukun@gmail.com>
2022-09-06 08:25:22 +0000

    :sparkles: Add RoomNotFound Exception

--- app/exceptions.py
+++ app/exceptions.py
@@ -6,12 +6,25 @@ class InvalidToken(Exception):
     """指定されたtokenが不正だったときに投げる"""
 
 
+class RoomNotFound(Exception):
+    """指定されたRoomが存在しない"""
+
+
 class InvalidJudgeResult(Exception):
     """プレイ結果が不正だったときに投げる"""
 
 
 def InvalidTokenHandler(request: Request, exception: InvalidToken) -> JSONResponse:
     return JSONResponse(status_code=401, content={"detail": "Token is invalid."})
+
+
+def RoomNotFoundHandler(request: Request, exception: RoomNotFound) -> JSONResponse:
+    return JSONResponse(
+        status_code=404,
+        content={
+            "detail": f"Room is not found. Room ID: {request.path_params['room_id']}"
+        },
+    )
 
 
 def InvalidJudgeResultHandler(

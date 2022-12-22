e2ffe1dc63cddb3c274bfba1aae3f64796528e67
Inada Naoki <songofacandy@gmail.com>
2022-12-22 10:00:55 +0000

    Remove Dockerfile

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+export DEBIAN_FRONTEND=noninteractive
+sudo apt-get update
+sudo apt-get -y install --no-install-recommends bash-completion \
+    default-libmysqlclient-dev default-mysql-client default-mysql-server \
+    vim
+
 python3 -m venv --prompt . venv
 venv/bin/pip install -U pip
 venv/bin/pip install -r requirements.txt

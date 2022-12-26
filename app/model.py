52861168740fdae22dcea25393afeef38f77fcfd
tesso <48358052+tesso57@users.noreply.github.com>
2022-12-26 06:37:15 +0000

    ⚡️ impl _get_user_by_token, update_user

--- app/model.py
+++ app/model.py
@@ -1,14 +1,28 @@
-import json
+# import json
 import uuid
-from enum import Enum, IntEnum
+
+# from enum import Enum, IntEnum
 from typing import Optional
 
-from fastapi import HTTPException
+# from fastapi import HTTPException
 from pydantic import BaseModel
-from sqlalchemy import text
+from sqlalchemy import select, update, text
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.exc import NoResultFound
+from sqlalchemy.orm import declarative_base
 
 from .db import engine
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = "user"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(255), nullable=True)
+    token = Column(String(255), nullable=True, unique=True)
+    leader_card_id = Column(Integer, nullable=True)
 
 
 class InvalidToken(Exception):
@@ -31,9 +45,15 @@ def create_user(name: str, leader_card_id: int) -> str:
     token = str(uuid.uuid4())
     # NOTE: tokenが衝突したらリトライする必要がある.
     with engine.begin() as conn:
-        result = conn.execute(
+        _ = conn.execute(
             text(
-                "INSERT INTO `user` (name, token, leader_card_id) VALUES (:name, :token, :leader_card_id)"
+                """
+                    INSERT
+                        INTO
+                            `user` (name, token, leader_card_id)
+                        VALUES
+                            (:name, :token, :leader_card_id)
+                """
             ),
             {"name": name, "token": token, "leader_card_id": leader_card_id},
         )
@@ -42,8 +62,13 @@ def create_user(name: str, leader_card_id: int) -> str:
 
 
 def _get_user_by_token(conn, token: str) -> Optional[SafeUser]:
-    # TODO: 実装
-    pass
+    res = conn.execute(select(User).where(User.token == token))
+    try:
+        row = res.one()
+    except NoResultFound:
+        return None
+    # return row
+    return SafeUser(id=row.id, name=row.name, leader_card_id=row.leader_card_id)
 
 
 def get_user_by_token(token: str) -> Optional[SafeUser]:
@@ -54,5 +79,14 @@ def get_user_by_token(token: str) -> Optional[SafeUser]:
 def update_user(token: str, name: str, leader_card_id: int) -> None:
     # このコードを実装してもらう
     with engine.begin() as conn:
-        # TODO: 実装
-        pass
+        return _update_user(conn=conn, token=token, name=name, leader_card_id=leader_card_id)
+
+
+def _update_user(conn, token: str, name: str, leader_card_id: int) -> None:
+    _ = conn.execute(
+        update(User).
+        where(User.token == token).
+        values(name=name, leader_card_id=leader_card_id)
+    )
+
+    return None

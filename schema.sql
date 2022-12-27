7b7d52c6700f424e28d25d4d6a018f6914ee8823
Masatoshi Kobayashi <61959411+hiremasa@users.noreply.github.com>
2022-12-27 11:19:40 +0000

    UPDATE: roomとroom_userを作成

--- schema.sql
+++ schema.sql
@@ -7,3 +7,27 @@ CREATE TABLE `user` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `token` (`token`)
 );
+
+
+DROP TABLE IF EXISTS `room`;
+CREATE TABLE `room` (
+  `room_id` bigint NOT NULL AUTO_INCREMENT,
+  `live_id` int NOT NULL,
+  `joined_user_count` int DEFAULT NULL,
+  `status` int DEFAULT NULL,
+  PRIMARY KEY (`room_id`)
+);
+
+
+DROP TABLE IF EXISTS `room_user`;
+CREATE TABLE `room_user` (
+  `room_id` bigint NOT NULL,
+  `user_id` bigint NOT NULL,
+  `user_name` varchar(255) NOT NULL,
+  `leader_card_id` int DEFAULT NULL,
+  `select_difficulty` int NOT NULL,
+  `is_host` boolean NOT NULL,
+  `score` int DEFAULT 0,
+  `end_playing` boolean NOT NULL DEFAULT false,
+  PRIMARY KEY (`room_id`, `user_id`)
+);

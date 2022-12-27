4fd486db63956a89fffa96890d6e64be10144215
kairi <kairi.satellite@gmail.com>
2022-12-27 01:57:02 +0000

    [update] _get_user_by_token, update_userの実装

--- app/model.py
+++ app/model.py
@@ -6,6 +6,7 @@ from typing import Optional
 from fastapi import HTTPException
 from pydantic import BaseModel
 from sqlalchemy import text
+from sqlalchemy.engine import Connection
 from sqlalchemy.exc import NoResultFound
 
 from .db import engine
@@ -31,9 +32,12 @@ def create_user(name: str, leader_card_id: int) -> str:
     token = str(uuid.uuid4())
     # NOTE: tokenが衝突したらリトライする必要がある.
     with engine.begin() as conn:
+        conn: Connection
+        while _get_user_by_token(conn, token) is not None:
+            token = str(uuid.uuid4())
         result = conn.execute(
             text(
-                "INSERT INTO `user` (name, token, leader_card_id) VALUES (:name, :token, :leader_card_id)"
+                "INSERT INTO `user` (`name`, `token`, `leader_card_id`) VALUES (:name, :token, :leader_card_id)"
             ),
             {"name": name, "token": token, "leader_card_id": leader_card_id},
         )
@@ -41,9 +45,16 @@ def create_user(name: str, leader_card_id: int) -> str:
     return token
 
 
-def _get_user_by_token(conn, token: str) -> Optional[SafeUser]:
-    # TODO: 実装
-    pass
+def _get_user_by_token(conn: Connection, token: str) -> Optional[SafeUser]:
+    result = conn.execute(
+        text("SELECT `id`, `name`, `leader_card_id` FROM `user` WHERE `token`=:token"),
+        {"token": token},
+    )
+    try:
+        row = result.one()
+    except NoResultFound:
+        return None
+    return SafeUser.from_orm(row)
 
 
 def get_user_by_token(token: str) -> Optional[SafeUser]:
@@ -52,7 +63,13 @@ def get_user_by_token(token: str) -> Optional[SafeUser]:
 
 
 def update_user(token: str, name: str, leader_card_id: int) -> None:
-    # このコードを実装してもらう
     with engine.begin() as conn:
-        # TODO: 実装
-        pass
+        conn: Connection
+        result = conn.execute(
+            text(
+                "UPDATE `user` SET `name`=:name, `leader_card_id`=:leader_card_id WHERE `token`=:token"
+            ),
+            {"name": name, "token": token, "leader_card_id": leader_card_id},
+        )
+        if result.rowcount != 1:
+            raise InvalidToken

18948a6f79cf60bd6599de27179db1fbec257db7
kairi <kairi.satellite@gmail.com>
2022-12-27 05:06:19 +0000

    [update] room API用テーブル定義

--- schema.sql
+++ schema.sql
@@ -7,3 +7,21 @@ CREATE TABLE `user` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `token` (`token`)
 );
+DROP TABLE IF EXISTS `room`;
+CREATE TABLE `room` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `live_id` int NOT NULL,
+  `select_difficulty` int NOT NULL DEFAULT 1,
+  `member_num` int NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`)
+);
+DROP TABLE IF EXISTS `room_member`;
+CREATE TABLE `room_member` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `user_id` bigint NOT NULL,
+  `room_id` bigint NOT NULL,
+  `is_host` boolean NOT NULL DEFAULT false,
+  FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
+  FOREIGN KEY (`room_id`) REFERENCES `room` (`id`),
+  PRIMARY KEY (`id`)
+);

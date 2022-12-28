f81cf8af34b02fd4dda99ed1f5b151e29b9932e1
20jun01 <56793204+20jun01@users.noreply.github.com>
2022-12-28 06:56:55 +0000

    ⚡️ add foreign key

--- schema.sql
+++ schema.sql
@@ -13,7 +13,7 @@ DROP TABLE IF EXISTS `room`;
 CREATE TABLE `room` (
   `id` bigint NOT NULL AUTO_INCREMENT,
   `live_id` int DEFAULT NULL,
-  `room_id` int DEFAULT NULL,
+  `room_id` bigint NOT NULL,
   `joined_user_count` int DEFAULT NULL,
   `max_user_count` int DEFAULT NULL,
   `status` int DEFAULT NULL,
@@ -31,6 +31,8 @@ CREATE TABLE `room_member` (
   `select_difficulty` int DEFAULT NULL,
   `is_host` boolean DEFAULT NULL,
   PRIMARY KEY (`room_id`, `user_id`),
+  CONSTRAINT `fk_1` FOREIGN KEY (`room_id`) REFERENCES `room` (`room_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   UNIQUE KEY `token` (`token`)
 );
 
@@ -45,5 +47,7 @@ CREATE TABLE `room_score` (
   `good_count` int DEFAULT NULL,
   `bad_count` int DEFAULT NULL,
   `miss_count` int DEFAULT NULL,
-  PRIMARY KEY (`room_id`, `user_id`)
+  PRIMARY KEY (`room_id`, `user_id`),
+  CONSTRAINT `fk_3` FOREIGN KEY (`room_id`) REFERENCES `room` (`room_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_4` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 );

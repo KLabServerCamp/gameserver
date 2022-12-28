642bf76ec62ba36bc60f54d360ed5ba322ff7166
kokhayas <58214906+kokhayas@users.noreply.github.com>
2022-12-28 02:45:08 +0000

    08-kokhayas schema.sqlのforeign keyを廃止した。複合keyを導入した

--- schema.sql
+++ schema.sql
@@ -16,16 +16,13 @@ CREATE TABLE `room` (
   `owner_id` bigint NOT NULL,
   `live_id` int NOT NULL,
   PRIMARY KEY (`id`),
-  FOREIGN KEY (`owner_id`) REFERENCES `user` (`id`)
 );
 
 CREATE TABLE `room_member` (
   `user_id` bigint NOT NULL,
   `room_id` bigint NOT NULL,
   `select_difficulty` int NOT NULL,
-  UNIQUE(`user_id`, `room_id`),
-  FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
-  FOREIGN KEY (`room_id`) REFERENCES `room` (`id`)
+  PRIMARY KEY (`room_id`, `user_id`)
 );
 
 

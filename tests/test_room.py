a44a951356bf6faef7ef04478d92862e5dc55e52
kairi <kairi.satellite@gmail.com>
2023-01-05 09:19:19 +0000

    testなど

--- tests/test_room.py
+++ tests/test_room.py
@@ -1,5 +1,6 @@
 from fastapi.testclient import TestClient
 
+from app import model
 from app.api import app
 
 client = TestClient(app)
@@ -23,10 +24,43 @@ def _auth_header(i=0):
     return {"Authorization": f"bearer {token}"}
 
 
+def _test_room_create(i, live_id=1001, difficulty=1):
+    response = client.post(
+        "/room/create",
+        headers=_auth_header(i),
+        json={"live_id": live_id, "select_difficulty": difficulty},
+    )
+    assert response.status_code == 200
+    room_id = response.json()["room_id"]
+    return room_id
+
+
+def _test_room_join(i, room_id, difficulty=1):
+    response = client.post(
+        "/room/join",
+        headers=_auth_header(i),
+        json={"room_id": room_id, "select_difficulty": difficulty},
+    )
+    assert response.status_code == 200
+    join_room_result = model.JoinRoomResult(response.json()["join_room_result"])
+    return join_room_result
+
+
+def _test_room_2():
+    room_id = _test_room_create(0)
+    room_id2 = _test_room_create(1)
+    assert room_id != room_id2
+    for i in range(2, 2 + 3):
+        join_result = _test_room_join(i, room_id)
+        assert join_result is model.JoinRoomResult.OK
+    join_result = _test_room_join(5, room_id)
+    assert join_result is model.JoinRoomResult.ROOM_FULL
+
+
 def test_room_1():
     response = client.post(
         "/room/create",
-        headers=_auth_header(1),
+        headers=_auth_header(),
         json={"live_id": 1001, "select_difficulty": 1},
     )
     assert response.status_code == 200
@@ -36,13 +70,13 @@ def test_room_1():
 
     response = client.post(
         "/room/join",
-        headers=_auth_header(),
+        headers=_auth_header(1),
         json={"room_id": room_id, "select_difficulty": 1},
     )
     assert response.status_code == 200
     print("room/join response:", response.json())
 
-    response = client.post(
+    """response = client.post(
         "/room/leave", headers=_auth_header(1), json={"room_id": room_id}
     )
     assert response.status_code == 200
@@ -50,7 +84,7 @@ def test_room_1():
 
     response = client.post("/room/list", headers=_auth_header(), json={"live_id": 1001})
     assert response.status_code == 200
-    print("room/list response:", response.json())
+    print("room/list response:", response.json())"""
 
     response = client.post(
         "/room/wait", headers=_auth_header(), json={"room_id": room_id}

24071b1a7e2bbf5ab3dabd4af5e717b02063e2cf
nerumae <48019587+nerumae@users.noreply.github.com>
2023-06-10 09:14:13 +0000

    schema.sqlを編集

--- schema.sql
+++ schema.sql
@@ -9,3 +9,23 @@ CREATE TABLE `user` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `token` (`token`)
 );
+
+DROP TABLE IF EXISTS `room`;
+CREATE TABLE `room` (
+  `room_id` int NOT NULL AUTO_INCREMENT,
+  `live_id` int NOT NULL,
+  `joined_user_count` int NOT NULL,
+  `max_user_count` int NOT NULL DEFAULT 4,
+  PRIMARY KEY (`room_id`)
+);
+
+DROP TABLE IF EXISTS `room_member`;
+CREATE TABLE `room_member` (
+  `user_id` bigint NOT NULL ,
+  `name` varchar(255) NOT NULL ,
+  `leader_card_id` int NOT NULL,
+  `select_diffculty` int NOT NULL,
+  `is_me` boolean NOT NULL,
+  `is_host` boolean NOT NULL
+  PRIMARY KEY (`user_id`)
+);

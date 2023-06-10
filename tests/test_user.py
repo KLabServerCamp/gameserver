f368dafbe627d01e878f60a3bcba71ce253dac2a
Kohei Noda <kohei-noda@hiroshima-u.ac.jp>
2023-06-10 07:47:04 +0000

    Add /user/update test

--- tests/test_user.py
+++ tests/test_user.py
@@ -23,3 +23,18 @@ def test_create_user():
     assert response_data.keys() == {"id", "name", "leader_card_id"}
     assert response_data["name"] == "test1"
     assert response_data["leader_card_id"] == 1000
+
+    new_name = "test2"
+    new_leader_card_id = 1001
+    response = client.post(
+        "/user/update",
+        json={"user_name": new_name, "leader_card_id": new_leader_card_id},
+        headers={"Authorization": f"bearer {token}"},
+    )
+    assert response.status_code == 200
+    response = client.get("/user/me", headers={"Authorization": f"bearer {token}"})
+    response_data = response.json()
+    print(f"response: {response.json()}")
+    assert response_data.keys() == {"id", "name", "leader_card_id"}
+    assert response_data["leader_card_id"] == new_leader_card_id
+    assert response_data["name"] == new_name

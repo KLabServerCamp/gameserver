4fc0dbdf118bf53cb367f4c6d4b43b587e9bf9a0
Kohei Noda <kohei-noda@hiroshima-u.ac.jp>
2023-06-10 06:43:31 +0000

    Implemented _get_user_by_token and update_user.

--- app/model.py
+++ app/model.py
@@ -43,8 +43,15 @@ def create_user(name: str, leader_card_id: int) -> str:
 
 
 def _get_user_by_token(conn, token: str) -> SafeUser | None:
-    # TODO: 実装(わからなかったら資料を見ながら)
-    ...
+    result = conn.execute(
+        text("SELECT `id`, `name`, `leader_card_id` FROM `user` WHERE `token`=:token"),
+        {"token": token},
+    )
+    try:
+        row = result.one()
+    except NoResultFound:
+        return None
+    return SafeUser.from_orm(row)
 
 
 def get_user_by_token(token: str) -> SafeUser | None:
@@ -54,8 +61,13 @@ def get_user_by_token(token: str) -> SafeUser | None:
 
 def update_user(token: str, name: str, leader_card_id: int) -> None:
     with engine.begin() as conn:
-        # TODO: 実装
-        ...
+        conn.execute(
+            text(
+                "UPDATE `user` SET `name`=:name, `leader_card_id`=:leader_card_id"
+                " WHERE `token`=:token"
+            ),
+            {"token": token, "name": name, "leader_card_id": leader_card_id},
+        )
 
 
 # IntEnum の使い方の例

ec5f45920f4b4d153b3bd83bb1c117f77705f5b5
fukumori-k <134461780+fukumori-k@users.noreply.github.com>
2023-07-14 06:27:18 +0000

    Delete: SQLスキーマからテスト用データを削除

--- schema.sql
+++ schema.sql
@@ -10,12 +10,6 @@ CREATE TABLE `user` (
   UNIQUE KEY `token` (`token`)
 );
 
-INSERT INTO `user` (id, name, token, leader_card_id) VALUES (51, "Taro", "7gOuy9Rr", "1001");
-INSERT INTO `user` (id, name, token, leader_card_id) VALUES (52, "Jiro", "Yy4ZERso", "1002");
-INSERT INTO `user` (id, name, token, leader_card_id) VALUES (53, "Saburo", "nBdQ0V1k", "1003");
-INSERT INTO `user` (id, name, token, leader_card_id) VALUES (54, "Yonro", "urF4rWPw", "1002");
-INSERT INTO `user` (id, name, token, leader_card_id) VALUES (55, "Goro", "0TVbIz00", "1002");
-
 DROP TABLE IF EXISTS `room`;
 CREATE TABLE `room` (
   `id` int NOT NULL AUTO_INCREMENT,
@@ -25,10 +19,6 @@ CREATE TABLE `room` (
   `is_game_started` boolean NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`)
 );
-
-INSERT INTO `room` (id, live_id, owner_id) VALUES (61, 1, 51);
-INSERT INTO `room` (id, live_id, owner_id) VALUES (62, 2, 52);
-INSERT INTO `room` (id, live_id, owner_id) VALUES (63, 3, 53);
 
 
 DROP TABLE IF EXISTS `room_member`;
@@ -45,8 +35,3 @@ CREATE TABLE `room_member` (
   `latest_num_miss` int DEFAULT NULL,
   PRIMARY KEY (`room_id`, `user_id`)
 );
-
-INSERT INTO `room_member` (room_id, user_id, select_difficulty) VALUES (61, 51, 1);
-INSERT INTO `room_member` (room_id, user_id, select_difficulty) VALUES (62, 52, 1);
-INSERT INTO `room_member` (room_id, user_id, select_difficulty) VALUES (61, 54, 1);
-INSERT INTO `room_member` (room_id, user_id, select_difficulty) VALUES (61, 55, 1);

c91c7dd07699ad30336b562b8b1273b37c1668f6
S <77969436+s21rd@users.noreply.github.com>
2023-09-07 03:06:59 +0000

    add schema of `room` table and `room_member` table

--- schema.sql
+++ schema.sql
@@ -9,3 +9,22 @@ CREATE TABLE `user` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `token` (`token`)
 );
+
+
+DROP TABLE IF EXISTS `room_member`;
+DROP TABLE IF EXISTS `room`;
+
+CREATE TABLE `room` (
+  `room_id` bigint NOT NULL AUTO_INCREMENT,
+  `owner_id` bigint NOT NULL,
+  `live_id` bigint NOT NULL,
+  PRIMARY KEY (`room_id`)
+);
+
+CREATE TABLE `room_member` (
+  `room_id` bigint NOT NULL,
+  `user_id` bigint NOT NULL,
+  `difficulty` int NOT NULL,
+  PRIMARY KEY (`room_id`, `user_id`),
+  FOREIGN KEY (`room_id`) REFERENCES `room` (`room_id`)
+);

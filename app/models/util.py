ebf0004b7cae3de2853fddbe3cd99c827636d718
S <77969436+s21rd@users.noreply.github.com>
2023-09-11 01:26:33 +0000

    add type annotation to conn

--- app/models/util.py
+++ app/models/util.py
@@ -1,9 +1,9 @@
-from sqlalchemy import text
+from sqlalchemy import Connection, text
 
 from app import schemas
 
 
-def _get_user_by_token(conn, token: str) -> schemas.SafeUser | None:
+def _get_user_by_token(conn: Connection, token: str) -> schemas.SafeUser | None:
     res = conn.execute(
         text("SELECT `id`, `name`, `leader_card_id` FROM `user` WHERE `token`=:token"),
         {"token": token},
@@ -14,7 +14,7 @@ def _get_user_by_token(conn, token: str) -> schemas.SafeUser | None:
     return schemas.SafeUser.model_validate(row, from_attributes=True)
 
 
-def _get_room_users_from_room_id(conn, room_id: int) -> list:
+def _get_room_users_from_room_id(conn: Connection, room_id: int) -> list:
     """room_id のルームに join しているプレイヤーを返す"""
     res = conn.execute(
         text("SELECT `user_id` FROM `room_member` WHERE `room_id`=:room_id"),
@@ -23,7 +23,7 @@ def _get_room_users_from_room_id(conn, room_id: int) -> list:
     return res.all()
 
 
-def _get_room_from_room_id(conn, room_id: int) -> schemas.SafeRoom | None:
+def _get_room_from_room_id(conn: Connection, room_id: int) -> schemas.SafeRoom | None:
     res = conn.execute(
         text(
             "SELECT `room_id`, `owner_id`, `live_id`, `max_user_count`, `status` FROM `room` WHERE `room_id`=:room_id"
@@ -36,7 +36,7 @@ def _get_room_from_room_id(conn, room_id: int) -> schemas.SafeRoom | None:
     return schemas.SafeRoom.model_validate(room, from_attributes=True)
 
 
-def _delete_user_from_room(conn, room_id: int, user_id: int) -> None:
+def _delete_user_from_room(conn: Connection, room_id: int, user_id: int) -> None:
     conn.execute(
         text(
             "DELETE FROM `room_member` WHERE `room_id`=:room_id AND `user_id`=:user_id"
@@ -45,7 +45,7 @@ def _delete_user_from_room(conn, room_id: int, user_id: int) -> None:
     )
 
 
-def _delete_room(conn, room_id) -> None:
+def _delete_room(conn: Connection, room_id) -> None:
     # room.status を 3 に変更
     conn.execute(
         text("UPDATE `room` SET `status`=3 WHERE `room_id`=:room_id"),
@@ -54,7 +54,7 @@ def _delete_room(conn, room_id) -> None:
 
 
 def _change_room_owner(
-    conn, room_id: int, owner: schemas.SafeUser, users_in_room: list
+    conn: Connection, room_id: int, owner: schemas.SafeUser, users_in_room: list
 ) -> None:
     # オーナーではない適当なユーザ 1 人にだけ owner 権限を移譲する
     for user in users_in_room:
@@ -67,7 +67,9 @@ def _change_room_owner(
         break
 
 
-def _update_room_status(conn, room_id: int, status: schemas.WaitRoomStatus) -> None:
+def _update_room_status(
+    conn: Connection, room_id: int, status: schemas.WaitRoomStatus
+) -> None:
     conn.execute(
         text("UPDATE `room` SET `status`=:status WHERE `room_id`=:room_id"),
         {"status": int(status), "room_id": room_id},
